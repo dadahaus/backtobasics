6bc58ffe10b3761c63c2151c228fea0334ba00a7
Agustin Sanchez Ferreyra <ostin@MacBook-Pro.local>
2022-10-04 13:36:28 -0300

    new change

--- advanced/OOP/script.js
+++ advanced/OOP/script.js
@@ -48,5 +48,60 @@ class Dog extends Animal{
     }
 }
 
+class Cat extends Animal {
+    constructor(name, breed) {
+        super(name)
+        this._breed = breed
+    }
+    get breed() {
+        return this._breed
+    }
+    speak() {
+        super.speak()
+        console.log(`${this.name} meows`)
+    }
+}
+
 // let simba = new Animal('simba')
 let simba = new Dog('simba', 'terrier')
+let machi = new Dog('thamachine', 'pitbull')
+let purr = new Cat('mishyy', 'nicey')
+
+let farm = [simba, machi, purr]
+for (a of farm) {
+    a.speak()
+}
+
+
+class Contractor {
+    constructor(name, role) {
+        this._name = name
+        this._role = role
+    }
+    get name() {
+        return this._name
+    }
+    get role() {
+        return this._role
+    }
+    sayHello() {
+        console.log(`hi there buckos and I am as a ${this._name}`)
+    }
+}
+
+class Backend extends Contractor {
+    constructor(name, role, tech) {
+        super(name, role)
+        this._tech = tech
+
+        get tech() {
+            return this._tech
+        }
+        sayHello() {
+            console.log(`hello, i am on the ${this._role} team at and I use ${this._tech}`)
+        }
+    }
+}
+
+let bob = new Contractor('Bob', 'front-end')
+let wachoo = new Backend('simba', 'font-end')
